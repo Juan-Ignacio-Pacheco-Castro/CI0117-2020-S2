b0817b8cbffcc84ac9fdaca2b0d6b270d09c8fa8
Juan Ignacio Pacheco Castro <juanipacas@gmail.com>
2020-09-08 15:51:01 -0600

    Added time difference in code of thread_creation_time

--- laboratorios/4/thread_creation_time/thread_creation_time.c
+++ laboratorios/4/thread_creation_time/thread_creation_time.c
@@ -1,7 +1,44 @@
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
-void main()
+void* vacio(void* args){
+}
+
+int main(int argc, char* arg[])
 {
-	printf("Hello World!");
+	unsigned int T = 1;
+	
+	if (argc == 2) {
+		T = (int) strtoul(arg[1], NULL, 10);
+	} else if (argc > 1) {
+		printf("Error, invalid number of parameters\n");
+		return 1;
+	}
+	
+	pthread_t secondary_thread;
+	clock_t start, stop;
+	double total;
+	double total_min = INFINITY;
+	
+	for (int test = 0; test < T; test++){
+		start = clock();
+		pthread_create(&secondary_thread, NULL, vacio, (void*)test);
+		stop = clock();
+		total = ((double)(stop-start))/CLOCKS_PER_SEC;
+		if (total < total_min){ 
+			total_min = total;
+		}
+	}
+	
+	printf("Minimun thread creation and destruction time was %lf s among %u trials\n", total_min, T);
 	return 0;
 }
+
+
+
+
+
+
