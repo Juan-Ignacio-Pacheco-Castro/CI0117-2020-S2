6a332a7cc923dbb0ecf829b71789e86e947397c0
Juan Ignacio Pacheco Castro <juanipacas@gmail.com>
2020-09-11 17:32:51 -0600

    Modified solution

--- laboratorios/[3]/[hello_ordered_mutex]/hello_ordered_mutex.c
+++ laboratorios/[3]/[hello_ordered_mutex]/hello_ordered_mutex.c
@@ -6,7 +6,6 @@
 
 typedef struct {
     size_t message;
-    size_t chose_thread;
     size_t next_thread;
     pthread_mutex_t* mutexes;
 } shared_message_t;
@@ -25,12 +24,8 @@ void* helloWorld(void* args) {
 
 	//Hacer lock. Manda a todos los threads a dormir
 	pthread_mutex_lock(&shared_message->mutexes[thread_num]);
-	
-	if (thread_num == shared_message->chose_thread) {
-        shared_message->message = 2021;
-    }
 
-    printf("Hello world from thread number # %zu. The message is: %zu\n", thread_num, shared_message->message);
+    printf("Hello world from thread number # %zu\n", thread_num);
 	
     ++shared_message->next_thread;
     //Unlock al siguiente thread
@@ -50,16 +45,9 @@ int main(int argc, char* arg[]) {
 	start = clock();
 
     size_t thread_count = 0;
-    size_t chosen_thread = 0;
 
-    if (argc >= 3) {
+    if (argc >= 2) {
         thread_count = (size_t)strtoul(arg[1], NULL, 10);
-        chosen_thread = (size_t)strtoul(arg[2], NULL, 10);
-
-        if (chosen_thread < 0 || chosen_thread >= thread_count) {
-            fprintf(stderr, "Error, invalid parameters\n");
-            return 1;
-        }
     } else {
         fprintf(stderr, "Error, invalid number of parameters\n");
         return 1;
@@ -70,7 +58,6 @@ int main(int argc, char* arg[]) {
     shared_message_t* shared_message = (shared_message_t*)calloc(1, sizeof(shared_message_t));
 
     shared_message->message = 2020;
-    shared_message->chose_thread = chosen_thread;
     shared_message->next_thread = 0;
     
     //Declaro lista de mutexes y la meto en Share_message
@@ -101,7 +88,7 @@ int main(int argc, char* arg[]) {
 
     //free(threads);
     //free(shared_message->mutexes);
-    //free(shared_message);
+    free(shared_message);
     //free(thread_data_list);
 
 	stop = clock();
