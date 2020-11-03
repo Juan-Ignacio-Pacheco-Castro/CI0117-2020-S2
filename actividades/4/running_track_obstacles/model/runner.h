d935d7e6d9432af151eeb6e9b88c1c43972cb8b2
Juan Ignacio Pacheco Castro <juanipacas@gmail.com>
2020-11-03 08:03:21 -0600

    Added activity 4

---
+++ actividades/4/running_track_obstacles/model/runner.h
@@ -0,0 +1,31 @@
+#ifndef RUNNER_H
+#define RUNNER_H
+
+#include "running_track.h"
+
+typedef struct runner
+{
+    int id;
+    int preparation_time;
+    int running_time;
+    int time_per_obstacle;
+} runner_t;
+
+
+typedef struct run_data
+{
+    runner_t* runner;
+    int * lane_start;
+    int * lane_finish;
+    int * position;
+    pthread_mutex_t * mutex_position;
+    pthread_barrier_t * barrier_start_line;
+    int** obstacle_matrix;
+} run_data_t;
+
+
+runner_t* runner_create(int id, int preparation_time, int running_time);
+void runner_destroy(runner_t* runner);
+void* run(void* args);
+
+#endif
