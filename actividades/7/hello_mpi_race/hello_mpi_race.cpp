b3b999902678d7072f0e967305210dfa15073694
Juan Ignacio Pacheco Castro <juanipacas@gmail.com>
2020-11-10 09:35:38 -0600

    Finished Activity 7

---
+++ actividades/7/hello_mpi_race/hello_mpi_race.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "mpi.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    int my_id, num_processes;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Status status;
+    int message_sent = 0;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+
+    int total_processes = num_processes-1;
+    if(!my_id){
+        while (total_processes){
+            total_processes--;
+            MPI_Recv(&message_sent, 1 /* count*/, MPI_INT, MPI_ANY_SOURCE /*source*/, 123 /*message id*/, MPI_COMM_WORLD, &status);
+            cout << "Process: " << message_sent << " finished. Total processes: " << num_processes << endl;
+        }
+    } else {
+        message_sent = my_id;
+        MPI_Send(&message_sent, 1 /*count*/, MPI_INT, 0 /*dest*/, 123 /*message id*/, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();
+
+    return 0;
+}
